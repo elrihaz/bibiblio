bc3b525cd5b6313a79b43300388ad097e338b940
elrihaz <elrihaz@gmail.com>
2018-07-07 09:44:08 +0200

    Parcour des fiches

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -13,14 +13,26 @@ export class SingleBookComponent implements OnInit {
 
   book: Book;
   id: number;
+  index: number;
+  maxIndex: number;
+  listBooks: Book[];
 
   constructor(  private booksService: BooksService,
                 private router: Router,
                 private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.listBooks = this.booksService.books.filter(
+      (el) => {
+        return (el.authorLastName.toLowerCase().indexOf(this.booksService.filterCritere.toLowerCase()) > -1 ||
+        el.authorFirstName.toLowerCase().indexOf(this.booksService.filterCritere.toLowerCase()) > -1 ||
+        el.title.toLowerCase().indexOf(this.booksService.filterCritere.toLowerCase()) > -1);
+        }
+    );
+    this.maxIndex = this.listBooks.length - 1;
     this.id = this.route.snapshot.params['id'];
     this.book = this.booksService.getSingleBook(+this.id);
+    this.index = this.listBooks.findIndex(BookEl => BookEl.id === +this.id);
   }
 
   onBack() {
@@ -29,6 +41,16 @@ export class SingleBookComponent implements OnInit {
 
   onEditBook(id: number) {
     this.router.navigate(['/books', 'edit', id]);
+  }
+
+  onNext() {
+    this.book = this.listBooks[this.index + 1];
+    this.index = this.listBooks.findIndex(BookEl => BookEl.id === +this.book.id);
+  }
+
+  onPrevious() {
+    this.book = this.listBooks[this.index - 1];
+    this.index = this.listBooks.findIndex(BookEl => BookEl.id === +this.book.id);
   }
 
 }
